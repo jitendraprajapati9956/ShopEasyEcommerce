75d5287c77f30406ab637d475ed9d8297b010eda
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-03-20 18:00:46 +0530

    Add files via upload

---
+++ Customer/pay_offline.php
@@ -0,0 +1,29 @@
+<div class="box"> <!-- video 51 start -->
+  <center>
+      <h1> Pay Offline Using Below Method </h1>
+      <p> If You Have Any Questions,Please Feel Free From To <a href="../contactus.php">
+         Contact Us </a>,Our Customer Service Center Is Working For You 24/7.
+      </p>
+  </center>
+  <hr>
+  <div class="table-responsive">
+      <table class="table table-bordered table-hover table-striped">
+          <thead>
+              <tr>
+                  <th> Bank Account Detail </th>
+                  <th> PayPal Id </th>
+                  <th> PayuMoney Link </th>
+                  <th> PayTm Number </th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td> 319701500927 </td>
+                  <td> chirag20019@gmail.com </td>
+                  <td> xxxxxxxxxxxxx </td>
+                  <td> 9328843957 </td>
+              </tr>
+          </tbody>
+      </table>
+  </div> <!-- video 51 end -->
+</div>
